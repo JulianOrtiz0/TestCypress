351b29d15b51d9daec47a7fd8278a4d3c48cbfe2
Julian Ortiz <julian.ortiz81@gmail.com>
2026-01-30 22:13:17 -0500

    Added cucumber.js

---
+++ cucumber.js
@@ -0,0 +1,6 @@
+module.exports = {
+  default: {
+    stepDefinitions: 'cypress/e2e/step_definitions/**/*.js',
+    require: 'cypress/e2e/step_definitions/**/*.js'
+  }
+};
--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,7 @@ const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor
 module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
-      await addCucumberPreprocessorPlugin(on, config, {
-        stepDefinitions: 'cypress/e2e/step_definitions/**/*.js'
-      });
+      await addCucumberPreprocessorPlugin(on, config);
       on("file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin(config)],
